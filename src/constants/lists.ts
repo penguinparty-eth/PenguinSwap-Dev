08e92664b28f3a043e2b250a308f1a296f48ab5a
johngulch <73127340+johngulch@users.noreply.github.com>
2020-11-14 17:45:00 -0500

    Update lists.ts
    use ipfs gateway for performance reasons.

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,7 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://gateway.pinata.cloud/ipfs/QmUPRJphaELm7JY2PLTtziN5PSrWpw692TY4DUsoL8KEb3'
+// export const DEFAULT_TOKEN_LIST_URL = 'https://gateway.pinata.cloud/ipfs/QmUPRJphaELm7JY2PLTtziN5PSrWpw692TY4DUsoL8KEb3'
+// cid or hashid is usable... a CID is added to ens, and becomes the hashid...
+export const DEFAULT_TOKEN_LIST_URL = 'https://gateway.ipfs.io/ipfs/bafybeicz3o277d52mgo2glmtvh2mrh5g2opdgadt2mocdpjcpiekuildfi'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
