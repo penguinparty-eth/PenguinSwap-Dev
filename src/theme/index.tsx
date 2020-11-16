465bf9595e33e09d6affc05613065cddc3766037
John Gulch <gulch@mailforce.net>
2020-11-15 21:41:14 -0500

    commiting the changes for the serviceworker background refresh.

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -222,7 +222,7 @@ html {
   linear-gradient(180deg, rgba(255,0,255,.1), rgba(0,255,255,0.1) 20%),
   linear-gradient(to bottom, transparent,transparent,transparent,transparent,#9999FF),
   linear-gradient(to top, transparent,transparent,transparent,lavender),
-  url('https://gateway.pinata.cloud/ipfs/QmSoJw2uMHaK6bNZZbqSQ6xZ7b37P4oGm5YFHqLqWibygq');
+  url('/images/penguini-snow.jpg');
   background-position: 0;
   background-repeat: no-repeat;
   background-attachment: fixed;
