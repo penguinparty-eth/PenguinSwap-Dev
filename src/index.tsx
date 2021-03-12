c75738ef27c39608ce49cc32801247e7b93a9321
Richard <richard@computer>
2021-03-11 23:51:03 -0500

    Add PWA service worker

--- src/index.tsx
+++ src/index.tsx
@@ -80,11 +80,23 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('worker.js').then(function(registration) {
+      console.log('Worker registration successful', registration.scope);
+    }, function(err) {
+      console.log('Worker registration failed', err);
+    }).catch(function(err) {
+      console.log(err);
+    });
+  });
+} else {
+  console.log('Service Worker is not supported by browser.');
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
