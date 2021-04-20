846774307c968d19a6f11c0ba6dc99e20781ddf9
Hiturunk <hiturunk@gmail.com>
2021-04-19 19:10:03 -0500

    I meant V5.6

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,7 +11,7 @@ export { PRELOADED_PROPOSALS } from './proposals'
 type ChainTokenList = {
   readonly [chainId in ChainId]: Token[]
 }
-export const VERSION = "5.5"
+export const VERSION = "5.6"
 export const DEFAULTTOKEN = '0x30bcd71b8d21fe830e493b30e90befba29de9114'
 export const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin')
 export const USDC = new Token(ChainId.MAINNET, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD//C')
