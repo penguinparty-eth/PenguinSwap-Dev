f157443c4e4467bfcd1560864997ad1c1aed2cfb
SanderNagtegaal <74963783+SanderNagtegaal@users.noreply.github.com>
2021-06-08 08:39:31 +0200

    Remove CoinGecko
    Remove CoinGecko because of performance issues that need to be fixed as soon as possible.

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -8,7 +8,6 @@ export const DEFAULT_TOKEN_LIST_URL = 'https://ipfs.io/ipfs/QmPzHxPByT8WCBSHsaBo
  */
 export const UNSUPPORTED_LIST_URLS: string[] = []
 const PENGUINLIST =  'https://ipfs.io/ipfs/QmPzHxPByT8WCBSHsaBoFHs929dysASm3mmwTLtCZknoGE'
-const LIDLIST =  'tokenlist.lid.eth'
 const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
 const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
 const AAVE_LIST = 'tokenlist.aave.eth'
@@ -17,7 +16,7 @@ const WRAPPED_LIST = 'wrapped.tokensoft.eth'
 const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
 const OPYN_LIST = 'https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json'
 const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
-const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
+//const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
 const CMC_ALL_LIST = 'defi.cmc.eth'
 const CMC_STABLECOIN = 'stablecoin.cmc.eth'
 const KLEROS_LIST = 't2crtokens.eth'
@@ -25,7 +24,6 @@ const KLEROS_LIST = 't2crtokens.eth'
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  LIDLIST,
   PENGUINLIST,
   COMPOUND_LIST,
   AAVE_LIST,
@@ -35,7 +33,7 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   SET_LIST,
   OPYN_LIST,
   ROLL_LIST,
-  COINGECKO_LIST,
+  //COINGECKO_LIST,
   CMC_ALL_LIST,
   CMC_STABLECOIN,
   KLEROS_LIST,
