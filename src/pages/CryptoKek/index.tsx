3d6f6769e2373ebdec0259b5ad8d6eeabbd0a2df
SanderNagtegaal <74963783+SanderNagtegaal@users.noreply.github.com>
2021-06-12 15:54:46 +0200

    Cleaning things up
    House keeping

--- src/pages/CryptoKek/index.tsx
+++ src/pages/CryptoKek/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-// Test token address
-const testTokenAddress = '0x30bcd71b8d21fe830e493b30e90befba29de9114'
-const cryptoKekLink = `https://keks.app/t/${testTokenAddress}/chart`
-// Import CryptoKek iFrame
-const cryptoKek = {
-  cryptokek: `<iframe src=${cryptoKekLink} width="100%" height="650px" frameborder="0" allowfullscreen="true" scrolling="no" align="center"> </iframe>`
-}
+// Test token address and link to the CryptoKekiFrame
+let testTokenAddress = '0x30bcd71b8d21fe830e493b30e90befba29de9114'
+const CRYPTO_KEK_LINK = `https://keks.app/t/${testTokenAddress}/chart`
+// CryptoKek iFrame
+const CRYPTO_KEK_IFRAME = `<iframe src=${CRYPTO_KEK_LINK} width="100%" height="650px" frameborder="0" allowfullscreen="true" scrolling="no" marginLeft: 100, marginRight: 100, marginBottom: 80, marginTop: 0 "> </iframe>`
 
 // Do magic so that it works in react
 function Iframe(props: { iframe: string }) {
@@ -15,8 +13,8 @@ function Iframe(props: { iframe: string }) {
 
 export default function CryptoKek() {
   return (
-    <div className="CryptoKek" style={{width: "80%", marginLeft: 100, marginRight: 100, marginBottom: 80, marginTop: 0 }}>
-      <Iframe iframe={cryptoKek['cryptokek']} />
+    <div className="CryptoKek" style={{width: "80%"}}>
+      <Iframe iframe={CRYPTO_KEK_IFRAME} />
     </div>
   )
 }
