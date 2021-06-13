0d85b5ed2a22f96484bb3459bc3569c7a74be705
Richard <richard@computer>
2021-06-13 15:35:21 -0400

    working embed, but it's squished and looks bad

--- src/components/CryptoKek/index.tsx
+++ src/components/CryptoKek/index.tsx
@@ -1,44 +1,47 @@
-import React from 'react'
-import { Currency, Token, WETH } from '@uniswap/sdk/dist/index'
+import React, {useState} from 'react'
+import { Currency } from '@uniswap/sdk/dist/index'
 import { useDerivedSwapInfo } from 'state/swap/hooks'
 import { Field } from 'state/swap/actions'
-
+import { WrappedTokenInfo } from '../../state/lists/hooks'
+import { USDC } from '../../constants'
 export default function CryptoKek() {
-  let testTokenAddress = '0x30bcd71b8d21fe830e493b30e90befba29de9114'
+  var [tokenAddress, setTokenAddress] = useState('0x30bcd71b8d21fe830e493b30e90befba29de9114')
   const { currencies } = useDerivedSwapInfo()
   let input = currencies[Field.INPUT]
   let output = currencies[Field.OUTPUT]
-  console.log({ input, output })
-  if (input?.symbol == Currency.ETHER.symbol) {
-    //WETH[1] should be WETH[current chain ID]
-    input = WETH[1]
-  }
-  if (output?.symbol == Currency.ETHER.symbol) {
-    output = WETH[1]
-  }
-  if (input instanceof Token) {
-    console.log('input address: ', input.address)
-  }
-  if (output instanceof Token) {
-    testTokenAddress = output.address
-  }
 
-  // Test token address and link to the CryptoKekiFrame
-  
-  const CRYPTO_KEK_LINK = `https://keks.app/t/${testTokenAddress}/chart`
-  console.log(CRYPTO_KEK_LINK)
-  
-  // CryptoKek iFrame
-  const CRYPTO_KEK_IFRAME = `<iframe src=${CRYPTO_KEK_LINK} width="100%" height="650px" frameborder="0" allowfullscreen="true" scrolling="no"> </iframe>`
-
-  // Do magic so that it works in react
-  function Iframe(props: { iframe: string }) {
-    return <div dangerouslySetInnerHTML={{ __html: props.iframe ? props.iframe : '' }} />
+  if(input instanceof WrappedTokenInfo && (!(output instanceof WrappedTokenInfo))){
+    if(input.address !== tokenAddress) {
+      setTokenAddress(input.address);
+    }
+  }
+  else if (output instanceof WrappedTokenInfo && tokenAddress !== output.address) {
+    setTokenAddress(output.address)
+  } else if(output instanceof Currency && input instanceof WrappedTokenInfo && tokenAddress !== input.address) {
+    setTokenAddress(input.address);
+  } else if(input === undefined || input === null || output === undefined || output === null) {
+    var singleField:Currency = null;
+    if(input instanceof Currency) {
+      singleField = input;
+    }
+    if(output instanceof Currency) {
+      singleField = output;
+    }
+    if(singleField !== null) {
+      if(singleField.symbol === "ETH" && tokenAddress !== USDC.address) {
+        setTokenAddress(USDC.address);
+      }
+    }
+  }
+  function kekFrame($token:string) {
+    const CRYPTO_KEK_LINK = `https://keks.app/t/${$token}/chart`
+    return {
+      __html: `<iframe src=${CRYPTO_KEK_LINK} width="100%" height="650px" frameBorder="0" allowFullScreen="true" scrolling="no"> </iframe>`
+    }
   }
 
   return (
-    <div className="CryptoKek" style={{ width: '80%' , marginLeft: 100, marginRight: 100, marginBottom: 80, marginTop: 0}}>
-      <Iframe iframe={CRYPTO_KEK_IFRAME} />
+    <div className="CryptoKek" dangerouslySetInnerHTML={kekFrame(tokenAddress)}>
     </div>
   )
 }
