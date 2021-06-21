16416e2c808eee0d83ce657a533364507c6ebd61
SanderNagtegaal <74963783+SanderNagtegaal@users.noreply.github.com>
2021-06-21 19:36:55 +0200

    Router fee included / some styling changes
    Router fee included / some styling changes

--- src/components/swap/AdvancedSwapDetails.tsx
+++ src/components/swap/AdvancedSwapDetails.tsx
@@ -3,7 +3,7 @@ import React, { useContext } from 'react'
 import { ThemeContext } from 'styled-components'
 import { Field } from '../../state/swap/actions'
 import { useUserSlippageTolerance } from '../../state/user/hooks'
-import { TYPE} from '../../theme'
+import { TYPE } from '../../theme'
 import { computeSlippageAdjustedAmounts, computeTradePriceBreakdown } from '../../utils/prices'
 import { AutoColumn } from '../Column'
 import QuestionHelper from '../QuestionHelper'
@@ -13,8 +13,6 @@ import SwapRoute from './SwapRoute'
 // import { useActiveWeb3React } from '../../hooks'
 import useUSDCPrice from '../../utils/useUSDCPrice'
 // import { UNI } from '../../constants'
-
-
 
 function TradeSummary({ trade, allowedSlippage }: { trade: Trade; allowedSlippage: number }) {
   const theme = useContext(ThemeContext)
@@ -26,65 +24,63 @@ function TradeSummary({ trade, allowedSlippage }: { trade: Trade; allowedSlippag
   const outputToken = trade.outputAmount.currency
   const usdcOutputPrice = useUSDCPrice(outputToken)
   const usdcInputPrice = useUSDCPrice(inputToken)
-  
 
-  
+  const totalInputUsdc =
+    parseFloat(slippageAdjustedAmounts[Field.INPUT].toExact()) * parseFloat(usdcInputPrice.toSignificant())
+  console.log(slippageAdjustedAmounts[Field.INPUT].toExact())
 
-  debugger;
+  const totalOutputUsdc =
+    parseFloat(slippageAdjustedAmounts[Field.OUTPUT].toExact()) * parseFloat(usdcOutputPrice.toSignificant())
+  console.log(totalOutputUsdc)
+
+  const routerFeePercentage = 0.0025
+  const routerFee = (totalInputUsdc * routerFeePercentage).toPrecision(2)
+  //debugger
   return (
-    <>
-      <AutoColumn style={{ padding: '0 16px' }}>
+    <div style={{ borderTop: '2px dotted grey' }}>
+      <AutoColumn style={{ padding: '1rem 16px' }}>
         <RowBetween>
           <RowFixed>
-          <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-            {'Rate'}
+            <TYPE.black fontSize={14} fontWeight={600} color={theme.text2}>
+              {'Rate'}
             </TYPE.black>
             <QuestionHelper text="This is the value of Token A as it relates to Token B" />
           </RowFixed>
           <RowFixed>
-          <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-          {'1 ' + inputToken.name + ' = ' + trade.executionPrice?.toFixed(2) + ' ' + outputToken.name}
-          </TYPE.black>
-          </RowFixed>
-        </RowBetween>
-        <RowBetween>
-          <RowFixed>
-          <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-            {'Inverse Rate'}
+            <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
+              {'1 ' + inputToken.name + ' = ' + trade.executionPrice?.toFixed(2) + ' ' + outputToken.name}
             </TYPE.black>
-            <QuestionHelper text="This is the value of Token B as it relates to Token A" />
-          </RowFixed>
-          <RowFixed>
-          <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-          {'1 ' + outputToken.name + ' = ' + trade.executionPrice.invert().toFixed(2) + ' ' + inputToken.name}
-          </TYPE.black>
           </RowFixed>
         </RowBetween>
         <RowBetween>
           <RowFixed>
-          <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-            {'USD Rate'}
+            <TYPE.black fontSize={14} fontWeight={600} color={theme.text2}>
+              {'USD Rate'}
             </TYPE.black>
             <QuestionHelper text="This is the value of token A in USDC" />
           </RowFixed>
           <RowFixed>
-          <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-          {'1 ' + inputToken.name + ' = $' + usdcInputPrice.toFixed(2)}
-          </TYPE.black>
+            <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
+              {'1 ' + inputToken.name + ' = $' + usdcInputPrice.toFixed(2)}
+            </TYPE.black>
           </RowFixed>
         </RowBetween>
         <RowBetween>
           <RowFixed>
-            <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
+            <TYPE.black fontSize={14} fontWeight={600} color={theme.text2}>
               {isExactIn ? 'Minimum received' : 'Maximum sold'}
             </TYPE.black>
             <QuestionHelper text="Your transaction will revert if there is a large, unfavorable price movement before it is confirmed." />
           </RowFixed>
           <RowFixed>
-            <TYPE.black color={theme.text1} fontSize={14}> 
+            <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
               {isExactIn
-                ? `${slippageAdjustedAmounts[Field.OUTPUT]?.toSignificant(4)} ${trade.outputAmount.currency.symbol} ${'/ $' + (Number(slippageAdjustedAmounts.OUTPUT.toSignificant(4)) * Number(usdcOutputPrice.toFixed(2))).toFixed(2)} ` ??
-                  '-'
+                ? `${slippageAdjustedAmounts[Field.OUTPUT]?.toSignificant(4)} ${
+                    trade.outputAmount.currency.symbol
+                  } ${'/ $' +
+                    (
+                      Number(slippageAdjustedAmounts.OUTPUT.toSignificant(4)) * Number(usdcOutputPrice.toFixed(2))
+                    ).toFixed(2)} ` ?? '-'
                 : `${slippageAdjustedAmounts[Field.INPUT]?.toSignificant(4)} ${trade.inputAmount.currency.symbol}` ??
                   '-'}
             </TYPE.black>
@@ -92,8 +88,8 @@ function TradeSummary({ trade, allowedSlippage }: { trade: Trade; allowedSlippag
         </RowBetween>
         <RowBetween>
           <RowFixed>
-            <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-              Price Impact
+            <TYPE.black fontSize={14} fontWeight={600} color={theme.text2}>
+              {'Price Impact'}
             </TYPE.black>
             <QuestionHelper text="The difference between the market price and estimated price due to trade size." />
           </RowFixed>
@@ -102,17 +98,28 @@ function TradeSummary({ trade, allowedSlippage }: { trade: Trade; allowedSlippag
 
         <RowBetween>
           <RowFixed>
-            <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-              Liquidity Provider Fee
+            <TYPE.black fontSize={14} fontWeight={600} color={theme.text2}>
+              {'Liquidity Provider Fee'}
             </TYPE.black>
             <QuestionHelper text="A portion of each trade (0.30%) goes to liquidity providers as a protocol incentive." />
           </RowFixed>
-          <TYPE.black fontSize={14} color={theme.text1}>
+          <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
             {realizedLPFee ? `${realizedLPFee.toSignificant(4)} ${trade.inputAmount.currency.symbol}` : '-'}
           </TYPE.black>
         </RowBetween>
+        <RowBetween>
+          <RowFixed>
+            <TYPE.black fontSize={14} fontWeight={600} color={theme.text2}>
+              {'Router Fee'}
+            </TYPE.black>
+            <QuestionHelper text="A portion of each trade (0.25%) goes to development of the platform." />
+          </RowFixed>
+          <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
+            {'$' + routerFee}
+          </TYPE.black>
+        </RowBetween>
       </AutoColumn>
-    </>
+    </div>
   )
 }
 
@@ -133,10 +140,10 @@ export function AdvancedSwapDetails({ trade }: AdvancedSwapDetailsProps) {
           <TradeSummary trade={trade} allowedSlippage={allowedSlippage} />
           {showRoute && (
             <>
-            <RowBetween style={{ padding: '0 16px' }}>
-              <span style={{ display: 'flex', alignItems: 'center' }}>
-                  <TYPE.black fontSize={14} fontWeight={400} color={theme.text2}>
-                    Route
+              <RowBetween style={{ padding: '0 1rem' }}>
+                <span style={{ display: 'flex', alignItems: 'center' }}>
+                  <TYPE.black fontSize={14} fontWeight={600} color={theme.text2}>
+                    {'Route'}
                   </TYPE.black>
                   <QuestionHelper text="Routing through these tokens resulted in the best price for your trade." />
                 </span>
